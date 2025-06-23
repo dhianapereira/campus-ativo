a3715ff8fbb363cd6903fb96b37d3d1025380281
dhiana <dhianaslv@outlook.com>
2025-06-22 23:25:08 -0300

    feat: Create database module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common'
-import { PrismaService } from '../prisma/prisma.service'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateProblemController } from './controllers/create-problem.controller'
 import { FetchProblemsController } from './controllers/fetch-problems.controller'
+import { DatabaseModule } from '../database/database.module'
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [
     CreateAccountController,
     AuthenticateController,
     CreateProblemController,
     FetchProblemsController,
   ],
-  providers: [PrismaService],
 })
 export class HttpModule {}
