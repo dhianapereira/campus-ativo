11dcf3401633af8c1e55439f4730fbd65759a078
dhiana <dhianaslv@outlook.com>
2025-06-24 00:15:11 -0300

    test: Fix unit tests

--- test/factories/make-problem.ts
+++ test/factories/make-problem.ts
@@ -3,7 +3,7 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import {
   Problem,
   ProblemProps,
-} from 'problem'
+} from '@/domain/maintenance-problems/enterprise/entities/problems/problem'
 
 export function makeProblem(
   override: Partial<ProblemProps> = {},
