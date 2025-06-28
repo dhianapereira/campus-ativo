6f4be150016faf8c206acbde74d9f8db67df9f4e
dhiana <dhianaslv@outlook.com>
2025-06-27 23:33:43 -0300

    feat: Create prisma attachments repository

---
+++ src/infra/database/prisma/mappers/prisma-problem-attachment-mapper.ts
@@ -0,0 +1,19 @@
+import { Attachment as PrismaAttachment } from '@prisma/client'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { ProblemAttachment } from '@/domain/maintenance-problems/enterprise/entities/problems/problem-attachment'
+
+export class PrismaProblemAttachmentMapper {
+  static toDomain(raw: PrismaAttachment): ProblemAttachment {
+    if (!raw.problemId) {
+      throw new Error('Invalid attachment type.')
+    }
+
+    return ProblemAttachment.create(
+      {
+        attachmentId: new UniqueEntityID(raw.id),
+        problemId: new UniqueEntityID(raw.problemId),
+      },
+      new UniqueEntityID(raw.id),
+    )
+  }
+}
--- src/infra/database/prisma/repositories/prisma-problem-attachments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-problem-attachments-repository.ts
@@ -1,16 +1,30 @@
 import { ProblemAttachmentsRepository } from '@/domain/maintenance-problems/application/repositories/problem-attachments-repository'
 import { ProblemAttachment } from '@/domain/maintenance-problems/enterprise/entities/problems/problem-attachment'
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../prisma.service'
+import { PrismaProblemAttachmentMapper } from '../mappers/prisma-problem-attachment-mapper'
 
 @Injectable()
 export class PrismaProblemAttachmentsRepository
   implements ProblemAttachmentsRepository
 {
-  findManyByProblemId(problemId: string): Promise<ProblemAttachment[]> {
-    throw new Error('Method not implemented.')
+  constructor(private prisma: PrismaService) {}
+
+  async findManyByProblemId(problemId: string): Promise<ProblemAttachment[]> {
+    const problemAttachments = await this.prisma.attachment.findMany({
+      where: {
+        problemId,
+      },
+    })
+
+    return problemAttachments.map(PrismaProblemAttachmentMapper.toDomain)
   }
 
-  deleteManyByProblemId(problemId: string): Promise<void> {
-    throw new Error('Method not implemented.')
+  async deleteManyByProblemId(problemId: string): Promise<void> {
+    await this.prisma.attachment.deleteMany({
+      where: {
+        problemId,
+      },
+    })
   }
 }
