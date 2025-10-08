7ef5b4142f8ffe8c96d0ef29e9fba9085fd0f5af
Alex Custódio <alex.scustod@gmail.com>
2025-10-08 14:57:49 -0300

    fix: remove do login verificação de regras de senha

--- src/infra/http/controllers/authenticate.controller.ts
+++ src/infra/http/controllers/authenticate.controller.ts
@@ -16,11 +16,7 @@ import { AuthenticateRequest, AuthenticateResponse } from '../dtos/interfaces.dt
 
 const authenticateBodySchema = z.object({
   email: z.string().email(),
-  password: z
-    .string()
-    .min(6, 'Password must be at least 6 characters')
-    .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
-    .regex(/[0-9]/, 'Password must contain at least one number'),
+  password: z.string(),
 })
 
 type AuthenticateBodySchema = z.infer<typeof authenticateBodySchema>
