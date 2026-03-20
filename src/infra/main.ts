d137a5506c61f8e78905f7edc1ecec194c361057
dhiana <dhianaslv@outlook.com>
2026-03-20 18:31:40 -0300

    fix: Make the openapi.json compatible with the frontend

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -17,9 +17,7 @@ async function bootstrap() {
         type: 'http',
         scheme: 'bearer',
         bearerFormat: 'JWT',
-        name: 'JWT',
         description: 'Enter JWT token',
-        in: 'header',
       },
       'JWT-auth',
     )
